b477d4896e8891054b21b0930cfa6ac150e83b8e
24pwbcs1437-boop <24pwbcs1437@uetpeshawar.edu.pk>
2026-06-19 16:17:32 +0500

    Update books.js

--- routes/books.js
+++ routes/books.js
@@ -18,5 +18,51 @@ router.post("/", async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+// GET single book by ID
+router.get("/:id", async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
 
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    res.json(book);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 module.exports = router;
+
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedBook = await Book.findByIdAndDelete(req.params.id);
+
+    if (!deletedBook) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    res.json({ message: "Book deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedBook = await Book.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true } // this returns updated data
+    );
+
+    if (!updatedBook) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    res.json(updatedBook);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
